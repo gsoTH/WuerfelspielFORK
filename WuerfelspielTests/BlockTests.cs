dba663213389d3894c420508fcffb5a76a192f53
M.Thomas <m.thomas@gso.schule.koeln>
2022-04-04 08:38:35 +0200

    Auskommentiert zwecks GUI Test

--- WuerfelspielTests/BlockTests.cs
+++ WuerfelspielTests/BlockTests.cs
@@ -7,33 +7,33 @@ namespace WuerfelspielTests
     [TestClass]
     public class BlockTests
     {
-        [TestMethod]
-        public void Block_KannErstelltWerden()
-        {
-            // Act
-            Block b = new Block();
+        //[TestMethod]
+        //public void Block_KannErstelltWerden()
+        //{
+        //    // Act
+        //    Block b = new Block();
 
-            // Assert
-            Assert.AreEqual(0, b.Summe);
-            foreach(Feld f in b.Felder)
-            {
-                Assert.AreEqual(0, f.Wert);
-            }
+        //    // Assert
+        //    Assert.AreEqual(0, b.Summe);
+        //    foreach(Feld f in b.Felder)
+        //    {
+        //        Assert.AreEqual(0, f.Wert);
+        //    }
 
-        }
+        //}
 
-        [TestMethod]
-        public void Eintragen_ÜbergibtWerteAnFelder()
-        {
-            // Arrange
-            Block b = new Block();
-            Wuerfel[] w = new Wuerfel[5];
+        //[TestMethod]
+        //public void Eintragen_ÜbergibtWerteAnFelder()
+        //{
+        //    // Arrange
+        //    Block b = new Block();
+        //    Wuerfel[] w = new Wuerfel[5];
 
-            // Act
+        //    // Act
 
-            // Assert
-            Assert.AreEqual(0, b.Summe);
+        //    // Assert
+        //    Assert.AreEqual(0, b.Summe);
 
-        }
+        //}
     }
 }
