64db0ecfbd2a1cda45756c455e0e9f4f9d8d5f74
M.Thomas <m.thomas@gso.schule.koeln>
2022-04-04 09:05:47 +0200

    Uc für Würfel hinzugefügt.

---
+++ Wuerfelspiel/Uc_Wuerfel.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Wuerfelspiel
+{
+    partial class Uc_Wuerfel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Uc_Wuerfel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Name = "Uc_Wuerfel";
+            this.Size = new System.Drawing.Size(174, 170);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Uc_Wuerfel_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Wuerfelspiel/Uc_Wuerfel.cs
@@ -0,0 +1,104 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Wuerfelspiel
+{
+    public partial class Uc_Wuerfel : UserControl
+    {
+        int wert = 0;
+        
+        public Uc_Wuerfel()
+        {
+            InitializeComponent();
+        }
+
+        public int Wert
+        {
+            set
+            {
+                this.wert = value;
+            }
+        }
+
+        private void Uc_Wuerfel_Paint(object sender, PaintEventArgs e)
+        {
+            //Hilfsvariablen
+            Graphics g = e.Graphics;
+            int h = this.Size.Height;
+            int w = this.Size.Width;
+
+            // Hier wird ein "Würfel" in mehrere Quadrate eingeteilt, um dort
+            // Punkte darzustellen. Je nach this.wert werden andere Quadrate ausgefüllt.
+            //+---+---+---+
+            //| 0 | 1 | 2 |
+            //+---+---+---+
+            //| 3 | 4 | 5 |
+            //+---+---+---+
+            //| 6 | 7 | 8 |
+            //+---+---+---+
+
+            Brush b = new SolidBrush(Color.Black);            
+            Rectangle[] rects = new Rectangle[9];
+            
+            int anteilW = w/3;
+            int anteilH = h/3;
+
+            int zaehler = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    rects[zaehler] = new Rectangle(j*anteilW, i*anteilH, anteilW, anteilH);
+                    zaehler++;
+                }
+            }
+            
+
+            switch (wert)
+            {
+                case 1:
+                    g.FillEllipse(b, rects[4]);
+                    break;
+                case 2:
+                    g.FillEllipse(b, rects[2]);
+                    g.FillEllipse(b, rects[6]);
+                    break;
+                case 3:
+                    g.FillEllipse(b, rects[0]);
+                    g.FillEllipse(b, rects[4]);
+                    g.FillEllipse(b, rects[8]);
+                    break;
+                case 4:
+                    g.FillEllipse(b, rects[0]);
+                    g.FillEllipse(b, rects[2]);
+                    g.FillEllipse(b, rects[6]);
+                    g.FillEllipse(b, rects[8]);
+                    break;
+                case 5:
+                    g.FillEllipse(b, rects[0]);
+                    g.FillEllipse(b, rects[2]);
+                    g.FillEllipse(b, rects[4]);
+                    g.FillEllipse(b, rects[6]);
+                    g.FillEllipse(b, rects[8]);
+                    break; 
+                case 6:
+                    g.FillEllipse(b, rects[0]);
+                    g.FillEllipse(b, rects[3]);
+                    g.FillEllipse(b, rects[6]);
+                    g.FillEllipse(b, rects[2]);
+                    g.FillEllipse(b, rects[5]);
+                    g.FillEllipse(b, rects[8]);
+                    break;
+                default:
+                    for (int i = 0; i < rects.Length; i++)
+                    {
+                        g.FillEllipse(b, rects[i]);
+                    }
+                    break;
+            }
+
+
+        }
+    }
+}
