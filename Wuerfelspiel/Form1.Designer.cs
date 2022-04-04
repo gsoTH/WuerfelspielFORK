93951b935f6ccfc8010a7d442c28220359ec31fc
M.Thomas <m.thomas@gso.schule.koeln>
2022-04-04 09:11:27 +0200

    Würfel auf Form dargestellt

--- Wuerfelspiel/Form1.Designer.cs
+++ Wuerfelspiel/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uc_Wuerfel1 = new Wuerfelspiel.Uc_Wuerfel();
+            this.uc_Wuerfel2 = new Wuerfelspiel.Uc_Wuerfel();
+            this.uc_Wuerfel3 = new Wuerfelspiel.Uc_Wuerfel();
+            this.SuspendLayout();
+            // 
+            // uc_Wuerfel1
+            // 
+            this.uc_Wuerfel1.BackColor = System.Drawing.SystemColors.Window;
+            this.uc_Wuerfel1.Location = new System.Drawing.Point(68, 65);
+            this.uc_Wuerfel1.Name = "uc_Wuerfel1";
+            this.uc_Wuerfel1.Size = new System.Drawing.Size(174, 170);
+            this.uc_Wuerfel1.TabIndex = 0;
+            // 
+            // uc_Wuerfel2
+            // 
+            this.uc_Wuerfel2.BackColor = System.Drawing.SystemColors.Window;
+            this.uc_Wuerfel2.Location = new System.Drawing.Point(314, 65);
+            this.uc_Wuerfel2.Name = "uc_Wuerfel2";
+            this.uc_Wuerfel2.Size = new System.Drawing.Size(174, 170);
+            this.uc_Wuerfel2.TabIndex = 1;
+            // 
+            // uc_Wuerfel3
+            // 
+            this.uc_Wuerfel3.BackColor = System.Drawing.SystemColors.Window;
+            this.uc_Wuerfel3.Location = new System.Drawing.Point(549, 65);
+            this.uc_Wuerfel3.Name = "uc_Wuerfel3";
+            this.uc_Wuerfel3.Size = new System.Drawing.Size(174, 170);
+            this.uc_Wuerfel3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uc_Wuerfel3);
+            this.Controls.Add(this.uc_Wuerfel2);
+            this.Controls.Add(this.uc_Wuerfel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Uc_Wuerfel uc_Wuerfel1;
+        private Uc_Wuerfel uc_Wuerfel2;
+        private Uc_Wuerfel uc_Wuerfel3;
     }
 }
 
